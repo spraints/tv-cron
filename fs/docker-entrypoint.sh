ee8ae4234a7868523f8e5be9207ae433a818f169
Matt Burke <spraints@gmail.com>
2026-09-11 21:40:53 -0400

    capture logs

--- fs/docker-entrypoint.sh
+++ fs/docker-entrypoint.sh
@@ -13,7 +13,7 @@ fi
 
 (
   printf 'PATH=/usr/bin:/bin\n'
-  printf '%s %s /opt/with-venv /opt/bin/sync-artwork.rb' \
+  printf '%s %s /opt/with-venv /opt/bin/sync-artwork.rb > /var/data/sync-artwork.log 2>&1' \
     "${CRON_SCHEDULE:-0 10 * * *}" "${CRON_USER:-root}"
   echo
 ) > /etc/cron.d/frame-art
