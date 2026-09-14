a0484019e126fe6f7a58751172931a69e243f366
Matt Burke <spraints@gmail.com>
2026-09-13 20:40:31 -0400

    MORE PRECISE

--- fs/docker-entrypoint.sh
+++ fs/docker-entrypoint.sh
@@ -6,8 +6,8 @@ set -eu
   printf 'PATH=/usr/bin:/bin\n'
   printf 'FRAME_TV_ADDR=%s\n' "$FRAME_TV_ADDR"
   printf 'DATA_DIR=%s\n' "$DATA_DIR"
-  printf '%s %s /opt/with-venv /opt/bin/sync-artwork.rb > /var/data/sync-artwork.log 2>&1' \
-    "${CRON_SCHEDULE:-0 10 * * *}" "${CRON_USER:-root}"
+  printf '%s %s /opt/with-venv /opt/bin/sync-artwork.rb > %s/sync-artwork.log 2>&1' \
+    "${CRON_SCHEDULE:-0 10 * * *}" "${CRON_USER:-root}" "${DATA_DIR}"
   echo
 ) > /etc/cron.d/frame-art
 
